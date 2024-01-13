084d7966b5bdf789718b996777dff07a1776fbda
aishwary saxena <aishwarysaxena993@gmail.com>
2024-01-13 15:54:28 +0530

    part e-1

---
+++ app.js
@@ -0,0 +1,19 @@
+const parent = React.createElement(
+  "div",
+  {id: "parent"},
+  React.createElement("div", {id: "child"}, [
+    React.createElement("h1", {id: "child1"}, "I am child1"),
+    React.createElement("h2", {id: "siblings"}, "I am sibling h2"),
+  ])
+);
+
+const heading = React.createElement(
+  "h1",
+  {id: "heading"},
+  "Hello world from react"
+);
+console.log(heading, typeof heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
